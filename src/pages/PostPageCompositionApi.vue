6deeb259ff64617c30e64d548cfb5910b11aeba9
Maksym <siroshtan.max1991ua@gmail.com>
2023-07-30 20:01:13 +0300

    Added page with Composition Api

---
+++ src/pages/PostPageCompositionApi.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <h1>Страница с постами</h1>
+    <my-input-vue
+      v-model="searchQuery"
+      placeholder="Поиск по названию...."
+      v-focus
+    />
+    <div class="app__btns">
+      <my-button-vue
+      >
+        Создать пост
+      </my-button-vue>
+      <my-select-vue
+        v-model="selectedSort"
+        :options="sortOptions"
+      />
+    </div>
+    <my-dialog-vue v-model:show="dialogVisible">
+      <post-form
+      />
+    </my-dialog-vue>
+    <post-list
+      :posts="sortedAndSearchedPosts"
+      v-if="!isPostsLoading"
+    />
+    <div v-else>Идет загрузка...</div>
+  </div>
+</template>
+
+<script>
+import PostForm from "../components/PostForm";
+import PostList from "../components/PostList";
+import {usePosts} from "../hooks/usePosts";
+import useSortedPosts from "../hooks/useSortedPosts";
+import useSortedAndSearchedPosts from "../hooks/useSortedAndSearchedPosts";
+export default {
+  components: {
+    PostList, PostForm
+  },
+  data() {
+    return {
+      dialogVisible: false,
+      sortOptions: [
+        {value: 'title', name: 'По названию'},
+        {value: 'body', name: 'По содержимому'},
+      ]
+    }
+  },
+  setup(props) {
+    const {posts, totalPages, isPostsLoading} = usePosts(10);
+    const {sortedPosts, selectedSort} = useSortedPosts(posts);
+    const {searchQuery, sortedAndSearchedPosts} = useSortedAndSearchedPosts(sortedPosts)
+
+    return {
+      posts,
+      totalPages,
+      isPostsLoading,
+      sortedPosts,
+      selectedSort,
+      searchQuery,
+      sortedAndSearchedPosts,
+    }
+  }
+}
+</script>
+
+<style>
+.app__btns {
+  margin: 15px 0;
+  display: flex;
+  justify-content: space-between;
+}
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+.current-page {
+  border: 2px solid teal;
+}
+.observer {
+  height: 30px;
+  background: green;
+}
+</style>
