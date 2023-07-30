c24eb9544c281c97a257d61790b26965ecf5d374
Maksym <siroshtan.max1991ua@gmail.com>
2023-07-30 14:28:31 +0300

    Added filter, sort and pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
-
+    <my-input-vue
+      placeholder="Найти по названию..."
+      v-model="searchQuery"
+    />
     <div class="app__btns">
       <my-button-vue @click="showDialog">
         Создать пост
@@ -18,11 +21,23 @@
     </my-dialog-vue>
 
     <post-list-vue
-      :posts="posts"
+      :posts="sortedAndSearchedPosts"
       @remove="removePost"
       v-if="!isPostLoading"
     />
     <div v-else>Идет загрузка...</div>
+    <div ref="observer" class="observer"></div>
+    <!-- <div class="page__wrapper">
+      <div
+        class="page"
+        v-for="pageNumber in totalPages"
+        :key="pageNumber"
+        :class="{
+          'current-page': pageNumber === page
+        }"
+        @click="changePage(pageNumber)"
+      >{{ pageNumber }}</div>
+    </div> -->
   </div>
 </template>
 
@@ -42,7 +57,11 @@ export default {
       posts: [],
       dialogVisible: false,
       isPostLoading: false,
+      searchQuery: "",
       selectedSort: "",
+      page: 1,
+      limit: 10,
+      totalPages: 0,
       sortOptions: [
         { value: "title", name: "По названию" },
         { value: "body", name: "По содержимому" },
@@ -65,8 +84,14 @@ methods: {
       try {
         this.isPostLoading = true;
         const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/posts?_limit=10"
+          "https://jsonplaceholder.typicode.com/posts", {
+            params: {
+              _page: this.page,
+              _limit: this.limit
+            }
+          }
         );
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
         this.posts = response.data;
       } catch (error) {
         console.error(error);
@@ -74,17 +99,59 @@ methods: {
         this.isPostLoading = false;
       }
     },
+    async loadMorePosts() {
+      try {
+        this.page += 1;
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts", {
+            params: {
+              _page: this.page,
+              _limit: this.limit
+            }
+          }
+        );
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
+        this.posts = [...this.posts, ...response.data];
+      } catch (error) {
+        console.error(error);
+      }
+    },
+/*     changePage(pageNumber) {
+      this.page = pageNumber;
+    } */
+  },
 
+  watch: {
+/*     page() {
+      this.fetchPosts();
+    } */
   },
 
   mounted() {
     this.fetchPosts();
-  },
 
-  watch: {
-    selectedSort(newValue) {
-      console.log(newValue);
+    const options = {
+      rootMargin: '0px',
+      threshold: 1.0
+    }
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting && this.page < this.totalPages) {
+        this.loadMorePosts();
+      }
+    };
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer);
     },
+
+  computed: {
+    sortedPost() {
+      return [...this.posts].sort((post1, post2) =>
+        post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+      )
+    },
+    sortedAndSearchedPosts() {
+      return this.sortedPost.filter((post) => post.title.toLowerCase().includes(this.searchQuery.toLocaleLowerCase()))
+    }
   },
 };
 </script>
@@ -102,5 +169,17 @@ methods: {
   margin: 15px 0;
   display: flex;
   justify-content: space-between;
+}
+.page__wrapper {
+  display: flex;
+  justify-content: center;
+  margin-top: 15px;
+}
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+.current-page {
+   border: 2px solid teal;
 }
 </style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length > 0">
     <h3>Список постов</h3>
-    <post-item-vue
+    <transition-group name="posts-list">
+      <post-item-vue
       v-for="post in posts"
       :key="post.id"
       :post="post"
       @remove="$emit('remove', post)"
     ></post-item-vue>
+    </transition-group>
   </div>
   <h2 v-else style="color: red">Список постов пуст.</h2>
 </template>
@@ -26,4 +28,21 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.posts-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.posts-list-enter-active,
+.posts-list-leave-active {
+  transition: all 0.6s ease;
+}
+.posts-list-enter-from,
+.posts-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.posts-list-move {
+  transition: transform 0.6s ease;
+}
+</style>
